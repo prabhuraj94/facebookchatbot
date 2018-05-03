a2b8dc89e09d2f12ece8005ba5f627effee927ef
Prabhu Raj Aranvoyal Rajan <prbhrj15@gmail.com>
2018-05-03 15:59:40 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/webhook/', function(req, res) {
-if (req.query['hub.verify_token'] === "facebooktestchatbot") {
+if (req.query['hub.verify_token'] === "facebookdemoechatbot") {
 res.send(req.query['hub.challenge'])
 }
+res.send('Error, no entry')
+});
 
 module.exports = router;
